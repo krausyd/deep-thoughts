78aadb068007b5c283f51f87c12f73d986f1b0f8
krausyd <krausyd@hotmail.com>
2022-02-07 20:07:39 -0800

    (∩◕-◕ )⊃━✿✿✿✿ Put some examples for the queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -50,5 +50,103 @@ type Mutation {
   }
 `;
 
+/* EXAMPLES OF QUERIES
+query {
+  me {
+    _id
+    username
+    email
+  }
+
+  # query all thoughts
+  thoughts {
+    _id
+    username
+    thoughtText
+    reactions {
+      _id
+      createdAt
+      username
+      reactionBody
+    }
+  }
+
+  # query a single thought, use the `_id` value of a thought that returned from a previous query
+  thought(_id: "6201d892c9c546408032fa9f") {
+    _id
+    username
+    thoughtText
+    createdAt
+    reactions {
+      username
+      reactionBody
+    }
+  }
+
+  #query all users
+  users {
+    _id
+    email
+    username
+  }
+
+  user(username: "Mellie74") {
+    email
+    username
+  }
+
+  
+}
+#mutation #addUser($username: String!, $password: String!, $email: String!) {
+ # addUser(username: $username, password: $password, email: $email) {
+ #   _id
+ #   username
+ #   email
+#  }
+#}
+#mutation login($email: String!, $password: String!) {
+#  login(email: $email, password: $password) {
+#    token
+ #   user {
+ #     username
+ #     _id
+ #     email
+ #   }
+ # }
+#}
+#mutation addThought($thoughtText: String!) {
+ # addThought(thoughtText: $thoughtText) {
+ #   _id
+ #   thoughtText
+ #   createdAt
+ #   username
+ #   reactionCount
+ # }
+#}
+#mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+ # addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+  #  _id
+   # reactionCount
+    #reactions {
+     # _id
+      #reactionBody
+      #createdAt
+      #username
+    #}
+  #}
+#}
+mutation addFriend($friendId: ID!) {
+  addFriend(friendId: $friendId) {
+    _id
+    username
+    friendCount
+    friends {
+      _id
+      username
+    }
+  }
+}
+*/
+
 // export the typeDefs
 module.exports = typeDefs;
